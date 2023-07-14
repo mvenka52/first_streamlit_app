1919439d6a5db731620da738dd3e399509ded087
mvenka52 <139536439+mvenka52@users.noreply.github.com>
2023-07-14 13:21:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My parents New Healthy Diner')
 
-streamlit.Header('Breakfast manu')
+streamlit.Header('Breakfast menu')
 streamlit.text('Breakfast1')
 streamlit.text('Breakfast2')
 streamlit.text('Breakfast3')
